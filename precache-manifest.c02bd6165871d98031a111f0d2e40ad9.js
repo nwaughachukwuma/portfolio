49f9ba1ea3a7cd624d5cf9464cfe8380bdcc0dcc
Chukwuma Nwaugha <c.nwaugha@gmail.com>
2019-12-31 13:41:33 +0100

    Updates

---
+++ precache-manifest.c02bd6165871d98031a111f0d2e40ad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40c1cca645fec5dd3bc16f15974e8e6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e4963fb16561ad736ad9",
+    "url": "/portfolio/static/css/main.a4e4088d.chunk.css"
+  },
+  {
+    "revision": "bf6f6d3e0c24ad4bc8b4",
+    "url": "/portfolio/static/js/2.a43d60fe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.a43d60fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4963fb16561ad736ad9",
+    "url": "/portfolio/static/js/main.9d59c395.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  }
+]);
