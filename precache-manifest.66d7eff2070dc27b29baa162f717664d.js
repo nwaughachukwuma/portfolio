6ee2b3c8e17171431e03063ab8c7a460bb81fc1c
Chukwuma Nwaugha <c.nwaugha@gmail.com>
2021-02-14 09:15:22 +0100

    Updates

--- precache-manifest.66d7eff2070dc27b29baa162f717664d.js
+++ precache-manifest.66d7eff2070dc27b29baa162f717664d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8dd6e6231f85506837d3b72e1c0425ab",
+    "revision": "7e8e1b910d3af461c0ce421d0226d4de",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "22c194af1d85c649524b",
+    "revision": "e88bb8d560012df707eb",
     "url": "/portfolio/static/css/main.a4e4088d.chunk.css"
   },
   {
-    "revision": "7f58c2061e6e9cde8cd2",
-    "url": "/portfolio/static/js/2.bc7995f8.chunk.js"
+    "revision": "e7bf7885680e567f0d30",
+    "url": "/portfolio/static/js/2.a3cd9b6a.chunk.js"
   },
   {
     "revision": "801eb6b718cfeb49e4699ab80a85e8cf",
-    "url": "/portfolio/static/js/2.bc7995f8.chunk.js.LICENSE"
+    "url": "/portfolio/static/js/2.a3cd9b6a.chunk.js.LICENSE"
   },
   {
-    "revision": "22c194af1d85c649524b",
-    "url": "/portfolio/static/js/main.ffbb1656.chunk.js"
+    "revision": "e88bb8d560012df707eb",
+    "url": "/portfolio/static/js/main.449151d2.chunk.js"
   },
   {
     "revision": "16c109c584451046bd84",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f83cc7f95d9ae07145b2eb742474e70c.js"
+  "/portfolio/precache-manifest.66d7eff2070dc27b29baa162f717664d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
