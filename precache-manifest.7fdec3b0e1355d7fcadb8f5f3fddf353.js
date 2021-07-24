6a289b0b275b9c56913c8143b75225e9e9197861
Chukwuma Nwaugha <nwaughac@gmail.com>
2021-07-24 22:05:09 +0400

    Updates

---
+++ precache-manifest.7fdec3b0e1355d7fcadb8f5f3fddf353.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7567fb42f26e021d20547f0803605846",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ed19a80af4903e78a838",
+    "url": "/portfolio/static/css/main.60068611.chunk.css"
+  },
+  {
+    "revision": "b4cb4d1eb5bc70418567",
+    "url": "/portfolio/static/js/2.7be06c08.chunk.js"
+  },
+  {
+    "revision": "b15b78ed56ee89efcee8578f85669660",
+    "url": "/portfolio/static/js/2.7be06c08.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed19a80af4903e78a838",
+    "url": "/portfolio/static/js/main.1a3ce449.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
