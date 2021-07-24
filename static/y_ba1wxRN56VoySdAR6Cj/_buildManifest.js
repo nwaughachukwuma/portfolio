9b6b345a73975088fd18d4abaa35a7adec5820a6
Chukwuma Nwaugha <nwaughac@gmail.com>
2021-07-24 21:56:36 +0400

    Updates

---
+++ static/y_ba1wxRN56VoySdAR6Cj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/298-8f085ed50291c8863f26.js","static/css/e2ccff8f5ddde8852a39.css","static/chunks/pages/index-05b10682bf88a3f525ac.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
