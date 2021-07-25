5aaddedf14a78c1c78bc89cf21519b01d5350057
nwaughachukwuma <nwaughac@gmail.com>
2021-07-25 13:11:58 +0000

    Deploying to gh-pages from @ nwaughachukwuma/portfolio@e81422f94268bf3e3dff4359f8f8c2d44d419b5b 🚀

---
+++ _next/static/ag0wRB-CZbUzCqiaj8IPw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/788-df4ef6e69993ae1b776b.js","static/css/e2ccff8f5ddde8852a39.css","static/chunks/pages/index-54a8b5a663a1796026f0.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
