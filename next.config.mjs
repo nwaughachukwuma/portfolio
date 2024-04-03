6c248812d5222dd23f755a5570dce62d6da08e31
Chukwuma Nwaugha <nwaughac@gmail.com>
2024-04-03 14:57:06 +0100

    Update assetPrefix in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-    // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
     output: 'export',
     // output: process.env.CI ? 'export' : undefined,
 };
