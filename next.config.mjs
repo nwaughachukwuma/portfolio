dea6fd2feb977bc27dae8393fe3dc8c9c880ea0e
Chukwuma Nwaugha <nwaughac@gmail.com>
2024-04-03 12:39:50 +0100

    Update next.config.mjs with basePath and assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+    output: 'export',
+    // output: process.env.CI ? 'export' : undefined,
 };
 
 export default nextConfig;
