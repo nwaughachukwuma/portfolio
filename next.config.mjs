c35260fea8a3777787c8e291092dc0f061678437
Chukwuma Nwaugha <nwaughac@gmail.com>
2024-04-03 15:08:07 +0100

    Update next.config.mjs and main.tsx

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,7 @@
 const nextConfig = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-    output: 'export',
-    // output: process.env.CI ? 'export' : undefined,
+    output: process.env.CI ? 'export' : undefined,
 };
 
 export default nextConfig;
