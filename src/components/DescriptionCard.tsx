3a11cafeca2e2e097af0e938f632eeef74f0f957
Chukwuma Nwaugha <nwaughac@gmail.com>
2024-04-13 19:07:55 +0100

    add content for projects and hobbies

---
+++ src/components/DescriptionCard.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+type Props = {
+  title: string;
+  children: React.ReactNode;
+  link?: string;
+  role?: string;
+};
+
+export function DescriptionCard({ title, children, link, role }: Props) {
+  return (
+    <div className="grid gap-1">
+      <h3 className="text-lg font-bold">{title}</h3>
+      <p className="text-sm text-gray-500 dark:text-gray-400">
+        {children}
+        {link ? (
+          <>
+            {" "}&minus;{" "}
+            <a href={link} target="_blank" className="text-blue-500">
+              {link.replace(/(^\w+:|^)\/\//, "")}
+            </a>
+          </>
+        ) : null}
+        <br />
+        <br />
+        {role ? (
+          <>
+            <span className="font-medium">Role:</span> {role}
+          </>
+        ) : null}
+      </p>
+    </div>
+  );
+}
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -9,6 +9,7 @@ import Footer from "./sections/footer"
 import Social from "./sections/social"
 import Image from "next/image"
 import {prefix} from "../utils/prefix"
+import {DescriptionCard} from "./DescriptionCard"
 
 export default function Main() {
   return (
@@ -42,9 +43,9 @@ export default function Main() {
                 <div className="my-4">
                   <Social />
                 </div>
-                <p className="mt-4 text-gray-500/60 animate-bounce">
+                {/* <p className="mt-4 text-gray-500/60 animate-bounce">
                   [Site rework In Progress]
-                </p>
+                </p> */}
               </div>
             </div>
           </div>
@@ -56,39 +57,62 @@ export default function Main() {
                 <div className="space-y-2">
                   <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">My Projects</h2>
                   <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                    Check out some of my recent projects. Click on a project to learn more.
+                    Recent ventures - click on the link to learn more.
                   </p>
                 </div>
               </div>
-              <div className="grid gap-4 min-[400px]:grid-cols-2 lg:order-last">
-                <div className="grid gap-1">
-                  <h3 className="text-lg font-bold">Project One</h3>
+              {/* <div className="grid gap-4 min-[400px]:grid-cols-2 lg:order-last"> */}
+              <div className="grid gap-4 md:gapy-4 gap-y-6 grid-cols-1 md:grid-cols-2 lg:order-last">
+               
+                {/* <div className="grid gap-1">
+                  <h3 className="text-lg font-bold">VeedoAI</h3>
                   <p className="text-sm text-gray-500 dark:text-gray-400">
-                    Description of project one. It was a challenging project that really allowed me to showcase my
-                    skills.
+                    {`VeedoAI is an AI-powered platform that provides deep insights for video content, using state of the art Generative AI and Large Language Models.`}
                   </p>
-                </div>
-                <div className="grid gap-1">
+                </div> */}
+
+                <DescriptionCard title="VeedoAI" role="Founder and Product Engineer" link="https://veedo.ai">
+                  {`AI-powered platform providing deep insights for video content using cutting-edge GenAI and LLMs`}
+                </DescriptionCard>
+
+
+                {/* <div className="grid gap-1">
                   <h3 className="text-lg font-bold">Project Two</h3>
                   <p className="text-sm text-gray-500 dark:text-gray-400">
                     {`Description of project two. I worked with a great team to bring this project to life and we're
                     really proud of the result.`}
                   </p>
-                </div>
-                <div className="grid gap-1">
+                </div> */}
+
+                <DescriptionCard title="Bible Links" role="Creator; AI Engineer" link="https://bible-links.vercel.app">
+                  {`Bible study companion providing multiple angles to the scriptures, powered by AI`}
+                </DescriptionCard>
+
+
+                {/* <div className="grid gap-1">
                   <h3 className="text-lg font-bold">Project Three</h3>
                   <p className="text-sm text-gray-500 dark:text-gray-400">
                     Description of project three. This project allowed me to experiment with some new technologies and I
                     learned a lot in the process.
                   </p>
-                </div>
-                <div className="grid gap-1">
+                </div> */}
+
+                <DescriptionCard title="Cayalx" role="Founder and Product Engineer" link="https://cayalx.com">
+                  {`Solving the bottleneck with current car hire and ride-share by putting riders & drivers in total control of their experience.`}
+                </DescriptionCard>
+
+
+                {/* <div className="grid gap-1">
                   <h3 className="text-lg font-bold">Project Four</h3>
                   <p className="text-sm text-gray-500 dark:text-gray-400">
                     Description of project four. I really enjoyed working on this project and I think it shows in the
                     final result.
                   </p>
-                </div>
+                </div> */}
+
+                <DescriptionCard title="Goldmine" role="Creator" link="https://goldmine-ai.vercel.app">
+                  {`Scrape the web and deliver current & relevant news, newsletters and articles all powered by AI.`}
+                </DescriptionCard>
               </div>
             </div>
           </div>
@@ -98,42 +122,59 @@ export default function Main() {
             <div className="mx-auto grid max-w-5xl items-start gap-6 lg:grid-cols-2 lg:gap-12">
               <div className="flex flex-col justify-center space-y-4">
                 <div className="space-y-2">
-                  <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">My Skills</h2>
+                  <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">Hobbies and Interests</h2>
                   <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                    {`I'm skilled in a variety of frontend technologies and tools. Here are a few of my areas of
-                    expertise.`}
+                    {`Things I do when I'm not building or experimenting with new technologies.`}
                   </p>
                 </div>
               </div>
-              <div className="grid gap-4 min-[400px]:grid-cols-2 lg:order-last">
-                <div className="grid gap-1">
+              {/* <div className="grid gap-4 min-[400px]:grid-cols-2 lg:order-last"> */}
+              <div className="grid gap-4 md:gapy-4 gap-y-6 grid-cols-1 md:grid-cols-2 lg:order-last">
+                {/* <div className="grid gap-1">
                   <h3 className="text-lg font-bold">HTML/CSS</h3>
                   <p className="text-sm text-gray-500 dark:text-gray-400">
                     {`I'm a master of the fundamentals. I can write clean, semantic HTML and style it with beautiful,
                     responsive CSS.`}
                   </p>
-                </div>
-                <div className="grid gap-1">
+                </div> */}
+
+                <DescriptionCard title="Technical Articles"  link="https://medium.com/@c.nwaugha">
+                  {`I love to share my knowledge and experiences with others. You can find my posts & articles on Medium and Notion`}
+                </DescriptionCard>
+
+                
+                {/* <div className="grid gap-1">
                   <h3 className="text-lg font-bold">JavaScript</h3>
                   <p className="text-sm text-gray-500 dark:text-gray-400">
                     {`I love working with JavaScript to create interactive and dynamic user experiences. I'm comfortable
                     with frameworks like React and Vue.`}
                   </p>
-                </div>
-                <div className="grid gap-1">
+                </div> */}
+
+                <DescriptionCard title="Hiking">
+                  {`I love to hike and explore outdoors. I find it's a great way to clear my mind and get some exercise.`}
+                </DescriptionCard>
+
+
+                {/* <div className="grid gap-1">
                   <h3 className="text-lg font-bold">Accessibility</h3>
                   <p className="text-sm text-gray-500 dark:text-gray-400">
                     I believe the web should be accessible to all. I make sure my sites are keyboard navigable, screen
                     reader friendly, and have high contrast modes.
                   </p>
-                </div>
-                <div className="grid gap-1">
+                </div> */}
+
+                <DescriptionCard title="Prayer and Meditation">
+                  {`I'm a Christian and Catholic. I love to pray and meditate to keep the words of God close to my heart.`}
+                </DescriptionCard>
+
+                {/* <div className="grid gap-1">
                   <h3 className="text-lg font-bold">Performance</h3>
                   <p className="text-sm text-gray-500 dark:text-gray-400">
                     I know the importance of a fast website. I optimize my assets, lazy load images, and use performance
                     budgets to keep my sites running smoothly.
                   </p>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
